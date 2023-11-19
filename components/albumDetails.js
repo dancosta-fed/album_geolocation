f569b36e1b806bc57ffe8b6b0c5ae60c0fdbec24
Daniel Costa <daniel@saudeagora.com.vc>
2023-11-19 10:31:53 -0300

    Saving progress. Only thing missing now is the modal with the photos

--- components/albumDetails.js
+++ components/albumDetails.js
@@ -24,40 +24,39 @@ const albumCovers = {
     link: "fotos_Stadium",
     location: "Belo Horizonte, MG",
     photos: [
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386212/IMG_4751_ywotuh.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386210/IMG_4745_vk0xzg.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386200/IMG_7427_x8soly.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386153/oaks-shore-51_inmkgp.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386148/oaks-shore-33_s1fykx.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386143/oaks-shore-29_i7bai2.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1691336520/IMG_2156_oyfnpj.png',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1691336422/B2B054D1-FFDE-461D-966D-A0AADE49932C_ms8yfv.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1691336468/_MG_4958_qtyhx0.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1691336039/IMG_8525_lb5fil.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1684164835/IMG_0693_obyj0t.png',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1684164789/IMG_1455_vg0bgq.png',
     ]
   },
   fotos_Blue_Pools: {
     imageUrl: "https://res.cloudinary.com/dognkye6x/image/upload/v1695431057/IMG_7721_oh8xmn.jpg",
-    title: "Blue Pools",
+    title: "South Island, NZ",
     description: "Fotos tiradas por Dan Costa no sul da NZ.",
     date: "2020",
-    link: "fotos_Blue_Pools",
-    location: "Haast, NZ",
+    link: "south_island",
+    location: "South Island, NZ",
     photos: [
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386212/IMG_4751_ywotuh.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386210/IMG_4745_vk0xzg.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386200/IMG_7427_x8soly.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386153/oaks-shore-51_inmkgp.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386148/oaks-shore-33_s1fykx.jpg',
-      'https://res.cloudinary.com/dognkye6x/image/upload/v1695386143/oaks-shore-29_i7bai2.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1695431057/IMG_7721_oh8xmn.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1695430685/GOPR0819_jel6u9.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1695430901/IMG_1911_hvctml.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1695430505/IMG_5256-Edit_r2ab1b.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1695430204/IMG_5616_gmhujj.jpg',
+      'https://res.cloudinary.com/dognkye6x/image/upload/v1695419277/IMG_2712_g7gl38.jpg',
     ]
   },
 };
 
-// Get URL parameters
 const params = new URLSearchParams(window.location.search);
 const title = params.get('link');
 const decodedLink = decodeURIComponent(title.replace(/\+/g, ' '));
 const selectedAlbum = albumCovers[decodedLink];
 
 const generateAlbumDetails = (album) => {
-  console.log(album);
+  console.log('album', album);
   return `
     <div class="bg-light p-2 rounded d-md-flex">
       <img src="${album.imageUrl}" class="rounded album-cover-page" alt="${album.title}">
@@ -83,12 +82,36 @@ const generateAlbumDetails = (album) => {
   `;
 }
 
+const generatePhotoHTML = (album) => {
+  let photosHTML = '';
+
+  album.photos.forEach((photo, index) => {
+    photosHTML += `
+      <div class="col-12 col-md-6 col-lg-4 mb-3">
+        <a href="#photoModal" data-bs-toggle="modal" data-bs-target="#photo${index + 1}">
+          <img src="${photo}" class="rounded foto-album" alt="snowboarder">
+        </a>
+      </div>
+    `;
+  });
+
+  return photosHTML;
+};
+
 const albumDetailsContainer = document.getElementById('albumDetailsContainer');
 
 console.log('selectedAlbum', selectedAlbum);
 if (selectedAlbum) {
+  const navLink = document.querySelector('.navbar-nav .nav-link');
+  navLink.textContent = selectedAlbum.title;
+  navLink.href = `albumDetails.html?link=${selectedAlbum.link}`;
+
   const albumDetailsHTML = generateAlbumDetails(selectedAlbum);
   albumDetailsContainer.innerHTML = albumDetailsHTML;
+
+  const photoGalleryHTML = generatePhotoHTML(selectedAlbum);
+  albumDetailsContainer.insertAdjacentHTML('beforeend', `<div class="row">${photoGalleryHTML}</div>`);
+
 } else {
   albumDetailsContainer.innerHTML = `
   <div class="d-flex justify-content-center align-items-center p-4">
